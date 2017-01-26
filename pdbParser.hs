5242d90a2e7319778f69cc0c1c1addf6deb26512
Max Tyler <mtyler88@gmail.com>
2017-01-26 19:33:07 +0000

    Add some data structures and some helpful functions for showing lines

---
+++ pdbParser.hs
@@ -0,0 +1,52 @@
+import Text.ParserCombinators.ReadP
+import Data.List (intercalate)
+data Line = End | Atoml Atom | Remarkl Remark deriving (Eq)
+
+instance Show Line where
+  show End = "END"
+  show (Atoml a) = show a
+  show (Remarkl a) = show a
+
+data Amino = ARG | HIS | LYS | ASP | GLU | SER | THR | ASN | GLN | CYS | SEC |
+           GLY | PRO | ALA | VAL | ILE | LEU | MET | PHE | TYR | TRP deriving (Show, Eq)
+
+data Remark = Remark {
+  remarkId :: Int,
+  remarkStr :: String
+                     } deriving (Eq)
+
+instance Show Remark where
+  show (Remark i s) = intercalate "\n" ["REMARK " ++ show i ++ " " ++ st | st <- lines s]
+
+data Atom = Atom {
+  atomId :: Int,
+  elementStr :: String,
+  amino :: Amino,
+  peptide :: String,
+  acidN :: Int,
+  x :: Double,
+  y :: Double,
+  z :: Double,
+  occupancy :: Double,
+  iso :: Double,
+  element :: String
+                 } deriving (Eq)
+
+instance Show Atom where
+  show (Atom i est am pe acid ax ay az occ is ele) = intercalate " " ["ATOM", show i, est,
+                                                                      show am, pe, show acid,
+                                                                      show ax, show ay, show az,
+                                                                      show occ, show is, ele]
+
+atString = "ATOM 3 HI GLY HO 20 0.2 0.423 1 2.023 -0.23 H"
+parsePDBLine :: ReadP Line
+parsePDBLine = undefined
+
+parseAtom :: ReadP String
+parseAtom = undefined
+
+main :: IO ()
+main = do
+  --print $ readP_to_S parseLine "ATOM POO"
+  --print $ Atoml $ Atom 3 "HI" GLY "HI" 30 0.22 0.22 0.22 1.00 0.00 "H"
+  --print $ Remarkl $ Remark 200 "Hello there\nI AM A TEST REMARK\n IS THIS WORKING?"
