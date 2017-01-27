a035e2110e596f2c89a8d7fdf0a6c91df1870691
Max Tyler <mtyler88@gmail.com>
2017-01-27 12:37:38 +0000

    Make parser read an atom string correctly and show an atom string correctly

--- pdbParser.hs
+++ pdbParser.hs
@@ -15,6 +15,18 @@ data Remark = Remark {
   remarkStr :: String
                      } deriving (Eq)
 
+justifyLeft :: Int -> Char -> String -> String
+justifyLeft i ch st
+  | lst >= i = st
+  | otherwise = st ++ (replicate (i-lst) ch)
+    where lst = length st
+
+justifyRight :: Int -> Char -> String -> String
+justifyRight i ch st
+  | lst >= i = st
+  | otherwise = replicate (i-lst) ch ++ st
+    where lst = length st
+
 instance Show Remark where
   show (Remark i s) = intercalate "\n" ["REMARK " ++ show i ++ " " ++ st | st <- lines s]
 
@@ -38,20 +50,51 @@ data Atom = Atom { -- Record Name 1 - 6 eg. "ATOM  "
                  } deriving (Eq)
 
 instance Show Atom where
+  show (Atom serialA nameA altLocA resNameA chainIDA resSeqA iCodeA xA yA zA
+       occupancyA tempFactorA segmentA elementA chargeA) =
+    let
+      rJstTake :: Int -> String -> String
+      rJstTake i str = justifyRight i ' ' $ take i str
+      lJstTake :: Int -> String -> String
+      lJstTake i str = justifyLeft i ' ' $ take i str
+      serSt = rJstTake 5 $ show serialA
+      namSt = rJstTake 4 $ nameA
+      altSt = rJstTake 1 $ [altLocA]
+      resSt = rJstTake 3 $ show resNameA
+      chaSt = rJstTake 1 $ [chainIDA]
+      rSqSt = rJstTake 4 $ show resSeqA
+      iCoSt = rJstTake 1 $ [iCodeA]
+      xSt = rJstTake 8 $ show xA
+      ySt = rJstTake 8 $ show yA
+      zSt = rJstTake 8 $ show zA
+      occSt = rJstTake 6 $ show occupancyA
+      temSt = rJstTake 6 $ show tempFactorA
+      segSt = lJstTake 4 $ segmentA
+      eleSt = rJstTake 2 $ elementA
+      chrSt = rJstTake 2 $ chargeA
+    in
+      "ATOM  " ++ serSt ++ " " ++ namSt ++ altSt ++ resSt ++ " " ++ chaSt ++ rSqSt ++ iCoSt ++
+      "   " ++ xSt ++ ySt ++ zSt ++ occSt ++ temSt ++ "      " ++ segSt ++ eleSt ++ chrSt
 
-atomFromString :: String -> Maybe Atom
-atomFromString str = case (take 6 str) of
-  "ATOM  " -> let
+atomFromString :: String -> Atom
+atomFromString str =
+  let
     tkDrpStrp :: Int -> Int -> String -> String
     tkDrpStrp i j st = filter ((/=)' ') $ take i $ drop j st
     tkDrpStrpPos i j st = tkDrpStrp (j-i+1) (i-1) st
     serialA = read (tkDrpStrpPos 7 11 str) :: Int
     nameA = tkDrpStrpPos 13 16 str
-    altLocA = head $ tkDrpStrpPos 17 17 str
+    altLocA = case tkDrpStrpPos 17 17 str of
+      c:cs -> c
+      _ -> ' '
     resNameA = read $ tkDrpStrpPos 18 20 str :: Amino
-    chainIDA = head $ tkDrpStrpPos 22 22 str
+    chainIDA = case tkDrpStrpPos 22 22 str of
+      c:cs -> c
+      _ -> ' '
     resSeqA = read $ tkDrpStrpPos 23 26 str :: Int
-    iCodeA = head $ tkDrpStrpPos 27 27 str
+    iCodeA = case tkDrpStrpPos 27 27 str of
+      c:cs -> c
+      _ -> ' '
     xA = read $ tkDrpStrpPos 31 38 str :: Double
     yA = read $ tkDrpStrpPos 39 46 str :: Double
     zA = read $ tkDrpStrpPos 47 54 str :: Double
@@ -60,13 +103,13 @@ atomFromString str = case (take 6 str) of
     segmentA = tkDrpStrpPos 73 76 str
     elementA = tkDrpStrpPos 77 78 str
     chargeA = tkDrpStrpPos 79 80 str
-    in
-      Just $ Atom serialA nameA altLocA resNameA chainIDA resSeqA iCodeA
-                     xA yA zA occupancyA tempFactorA segmentA elementA chargeA
-  _ -> Nothing
+  in
+    Atom serialA nameA altLocA resNameA chainIDA resSeqA iCodeA
+    xA yA zA occupancyA tempFactorA segmentA elementA chargeA
 
 
-atString = "ATOM 3 HI GLY HO 20 0.2 0.423 1 2.023 -0.23 H"
+atString = "ATOM  32    HI  OGLY H 20 A   0.423   1 2.023 -0.23 H"
+atoom = Atom 3 "AO020" 'p' GLY '2' 30 'a' 10.2030 30.2010 0.2313 0.23 0.0 "hol" "aoa" "hi"
 
 mergeRemarks :: Remark -> Remark -> Remark
 mergeRemarks (Remark i s1) (Remark _ s2) = Remark i (s1++"\n"++s2)
@@ -93,6 +136,6 @@ processPDBString str = intercalate "\n" $ reNumberAtoms (removeHydrogens (lines 
 
 main :: IO ()
 main = do proteinFile <- readFile "/home/max/mtyler88@gmail.com/University/4 Fourth Year/Senior Honours Project/Materials/protein/2wgo1.pdb"
-          fstLine <- return (head  $ lines proteinFile)
-          putStrLn fstLine
-          print $ liftM element $ atomFromString $ fstLine
+          let fstAtom = take 500  $ lines proteinFile
+          let at = map atomFromString fstAtom
+          mapM_ print at
