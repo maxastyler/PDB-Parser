b378e6ca053f7899b568d7f54f35b97a5a1005c4
Max Tyler <mtyler88@gmail.com>
2017-02-16 01:34:22 +0000

    Added renumbering residues to filter file

--- src/filterFile.hs
+++ src/filterFile.hs
@@ -5,7 +5,7 @@ import Control.Monad (liftM)
 main :: IO ()
 main = do filePath <- liftM head getArgs
           proteinFile <- readFile filePath
-          let ats = reNumAtoms (filter (\at -> altLoc at == 'A' || altLoc at == ' ' ) $ extractAtoms proteinFile) 1
+          let ats = reNumAtoms (reNumResidues (filter (\at -> head (name at) /= 'H') $ extractAtoms proteinFile) 1) 1
           let ats' = map (\at -> at{altLoc=' ', occupancy=1.0}) ats
           mapM_ print ats'
           putStrLn "END"
