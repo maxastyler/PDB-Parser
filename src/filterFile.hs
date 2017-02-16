4eef170c7ec09a92971f8e27704fb6afa7c82df0
Max Tyler <mtyler88@gmail.com>
2017-02-16 01:13:12 +0000

    Edit filter

--- src/filterFile.hs
+++ src/filterFile.hs
@@ -5,6 +5,7 @@ import Control.Monad (liftM)
 main :: IO ()
 main = do filePath <- liftM head getArgs
           proteinFile <- readFile filePath
-          let ats = reNumAtoms (filter (\at -> element at /= "H" && resSeq at >= 1 ) $ extractAtoms proteinFile) 1
-          mapM_ print ats
+          let ats = reNumAtoms (filter (\at -> altLoc at == 'A' || altLoc at == ' ' ) $ extractAtoms proteinFile) 1
+          let ats' = map (\at -> at{altLoc=' ', occupancy=1.0}) ats
+          mapM_ print ats'
           putStrLn "END"
